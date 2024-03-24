cad356282d9dfd9b04768804dfd9b08ee47b9112
Anton Deguet <anton.deguet@jhu.edu>
2024-03-24 16:50:40 -0400

    synched ROS and non ROS Qt examples

--- core/examples/ConsoleQt/main.cpp
+++ core/examples/ConsoleQt/main.cpp
@@ -42,17 +42,19 @@ int main(int argc, char * argv[])
     // parse options
     cmnCommandLineOptions options;
     std::string jsonConfigFile = "";
+    std::list<std::string> managerConfig;
 
     options.AddOptionOneValue("j", "json-config",
                               "json configuration file",
                               cmnCommandLineOptions::REQUIRED_OPTION, &jsonConfigFile);
+    options.AddOptionMultipleValues("m", "component-manager",
+                                    "JSON files to configure component manager",
+                                    cmnCommandLineOptions::OPTIONAL_OPTION, &managerConfig);
     options.AddOptionNoValue("D", "dark-mode",
                              "replaces the default Qt palette with darker colors");
 
-    std::string errorMessage;
-    if (!options.Parse(argc, argv, errorMessage)) {
-        std::cerr << "Error: " << errorMessage << std::endl;
-        options.PrintUsage(std::cerr);
+    // check that all required options have been provided
+    if (!options.Parse(argc, argv, std::cerr)) {
         return -1;
     }
 
@@ -90,6 +92,12 @@ int main(int argc, char * argv[])
     componentManager->Connect(systemWidget->GetName(), "Component",
                               galilController->GetName(), "control");
     tabWidget->addTab(systemWidget, "System");
+
+    // custom user components
+    if (!componentManager->ConfigureJSON(managerConfig)) {
+        CMN_LOG_INIT_ERROR << "Configure: failed to configure component-manager, check cisstLog for error messages" << std::endl;
+        return -1;
+    }
 
     // create and start all components
     componentManager->CreateAllAndWait(5.0 * cmn_s);
